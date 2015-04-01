69ba093fc474dcb613438e326b1a76419ec35dca
Mohammad Danish Siddiqui <mohammaddsiddiqui@gmail.com>
2015-04-02 01:33:21 +0500

    initialcommit

---
+++ InText/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace InText
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbDirectory = new System.Windows.Forms.ComboBox();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbResults = new System.Windows.Forms.ListBox();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.pbSearchProgress = new System.Windows.Forms.ProgressBar();
+            this.btnParallelSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbDirectory
+            // 
+            this.cbDirectory.FormattingEnabled = true;
+            this.cbDirectory.Location = new System.Drawing.Point(98, 36);
+            this.cbDirectory.Name = "cbDirectory";
+            this.cbDirectory.Size = new System.Drawing.Size(102, 21);
+            this.cbDirectory.TabIndex = 13;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Location = new System.Drawing.Point(9, 40);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(82, 13);
+            this.lblSelect.TabIndex = 12;
+            this.lblSelect.Text = "Select Directory";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(437, 399);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(93, 23);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop Searching";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbResults
+            // 
+            this.lbResults.FormattingEnabled = true;
+            this.lbResults.HorizontalExtent = 1;
+            this.lbResults.HorizontalScrollbar = true;
+            this.lbResults.Location = new System.Drawing.Point(12, 65);
+            this.lbResults.Name = "lbResults";
+            this.lbResults.Size = new System.Drawing.Size(518, 329);
+            this.lbResults.TabIndex = 10;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(206, 6);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(95, 23);
+            this.btSearch.TabIndex = 9;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(98, 8);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(102, 20);
+            this.txtSearch.TabIndex = 8;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(9, 11);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(71, 13);
+            this.lblSearch.TabIndex = 7;
+            this.lblSearch.Text = "Search String";
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Location = new System.Drawing.Point(206, 34);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(95, 23);
+            this.btnSelectDirectory.TabIndex = 14;
+            this.btnSelectDirectory.Text = "Select Directory";
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.btnSelectDirectory_Click);
+            // 
+            // pbSearchProgress
+            // 
+            this.pbSearchProgress.Location = new System.Drawing.Point(13, 399);
+            this.pbSearchProgress.Name = "pbSearchProgress";
+            this.pbSearchProgress.Size = new System.Drawing.Size(418, 23);
+            this.pbSearchProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbSearchProgress.TabIndex = 15;
+            // 
+            // btnParallelSearch
+            // 
+            this.btnParallelSearch.Location = new System.Drawing.Point(308, 6);
+            this.btnParallelSearch.Name = "btnParallelSearch";
+            this.btnParallelSearch.Size = new System.Drawing.Size(87, 23);
+            this.btnParallelSearch.TabIndex = 16;
+            this.btnParallelSearch.Text = "Parallel Search";
+            this.btnParallelSearch.UseVisualStyleBackColor = true;
+            this.btnParallelSearch.Click += new System.EventHandler(this.btnParallelSearch_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 431);
+            this.Controls.Add(this.btnParallelSearch);
+            this.Controls.Add(this.pbSearchProgress);
+            this.Controls.Add(this.btnSelectDirectory);
+            this.Controls.Add(this.cbDirectory);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lbResults);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Name = "frmMain";
+            this.Text = "Search in Files";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbDirectory;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbResults;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.ProgressBar pbSearchProgress;
+        private System.Windows.Forms.Button btnParallelSearch;
+    }
+}
+
---
+++ InText/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InText
+{
+    public partial class frmMain : Form
+    {
+
+        List<String> directories;
+        Thread parallel;
+        bool isSearching;
+        DateTime startTime;
+        DateTime endTime;
+        public frmMain()
+        {
+            InitializeComponent();
+            directories = new List<string>();
+            parallel = new Thread(searchParallel);
+            isSearching = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                cbDirectory.Items.Add(drive.ToString());
+            }
+        }
+
+        private void btnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            DialogResult folder = folderBrowserDialog.ShowDialog();
+            if (folder == DialogResult.OK)
+            {
+                cbDirectory.Text = folderBrowserDialog.SelectedPath.ToString();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isSearching = false;
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            isSearching = true;
+            
+            directories.Clear();
+            lbResults.Items.Clear();
+            pbSearchProgress.Value = 0;
+            
+            if (cbDirectory.Text.Length != 0)
+            {
+                getAllDirectories(cbDirectory.Text);
+                pbSearchProgress.Maximum = directories.Count;
+                pbSearchProgress.Minimum = 0;
+                searchNoParallel();
+            }
+
+        }
+
+        private void searchNoParallel()
+        {
+            startTime = DateTime.Now;
+                foreach (String dir in directories)
+                {
+                    //MessageBox.Show(dir.ToString());
+                    String[] files = Directory.GetFiles(dir);
+
+                    foreach (String file in files)
+                    {
+                        //MessageBox.Show(file.ToString());
+                        
+                        if (checkFilePermission(file))
+                        {
+                            StreamReader reader = new StreamReader(file);
+                            //MessageBox.Show(file);
+                            int lineCount = 0;
+                            while (reader.Peek() != -1)
+                            {
+                                Application.DoEvents();
+                                String line = reader.ReadLine();
+                                lineCount++;
+                                if (line.ToLower().Contains(txtSearch.Text.ToLower()))
+                                {
+                                    lbResults.Items.Add("Found in " + file + " at line: " + lineCount);
+                                }
+                                if (isSearching == false)
+                                {
+                                    pbSearchProgress.Value = 0;
+                                    break;
+                                }
+                            }
+                            reader.Close();
+                        }
+                        if (isSearching == false)
+                        {
+                            break;
+                        }
+                    }
+                    pbSearchProgress.Value++;
+                    if (isSearching == false)
+                    {
+                        break;
+                    }
+                }
+            pbSearchProgress.Value = 0;
+            endTime = DateTime.Now;
+            MessageBox.Show("Time Taken: " + endTime.Subtract(startTime).TotalMilliseconds + " Miliseconds");
+
+        }
+
+        private void btnParallelSearch_Click(object sender, EventArgs e)
+        {
+            isSearching = true;
+            directories.Clear();
+            lbResults.Items.Clear();
+            //searchParallel();
+            
+            if (cbDirectory.Text.Length != 0)
+            {
+                if (parallel.ThreadState == ThreadState.Stopped || parallel.ThreadState == ThreadState.Aborted) parallel.Abort();
+                
+                getAllDirectories(cbDirectory.Text);
+                pbSearchProgress.Maximum = directories.Count;
+                pbSearchProgress.Minimum = 0;
+                pbSearchProgress.Value = 0;
+                parallel.Start();
+            }
+            
+        }
+
+        private void searchParallel()
+        {
+                //foreach (String dir in directories)
+            ParallelOptions opt = new ParallelOptions();
+            opt.MaxDegreeOfParallelism = 14;
+            startTime = DateTime.Now;
+                Parallel.ForEach(directories,opt, (dir,loopState) =>
+                {
+                        //MessageBox.Show(dir.ToString());
+                        String[] files = Directory.GetFiles(dir);
+
+                        foreach (String file in files)
+                        {
+                            //MessageBox.Show(file.ToString());
+
+                            if (checkFilePermission(file))
+                            {
+                                StreamReader reader = new StreamReader(file);
+                                //MessageBox.Show(file);
+                                int lineCount = 0;
+                                while (reader.Peek() != -1)
+                                {
+                                    //Application.DoEvents();
+                                    String line = reader.ReadLine();
+                                    lineCount++;
+                                    if (line.ToLower().Contains(txtSearch.Text.ToLower()))
+                                    {
+                                        updateList(file, lineCount);
+                                    }
+                                    if (isSearching == false)
+                                    {
+                                        parallel.Abort();
+
+                                    }
+                                }
+                                reader.Close();
+                            }
+                            //if (isSearching == false)
+                            //{
+                            //    break;
+                            //}
+                        }
+                        pbMakeProgress();
+                        if (isSearching == false)
+                        {
+                            loopState.Stop();
+                        }
+                        
+                });
+                //MessageBox.Show("Search Completed.");
+                endTime = DateTime.Now;
+                MessageBox.Show("Time Taken: " + endTime.Subtract(startTime).TotalMilliseconds + " Miliseconds");
+                isSearching = false;
+                pbMakeProgress();
+
+        }
+
+        private void getAllDirectories(String driveRootDirectory)
+        {
+            String[] dirs = Directory.GetDirectories(driveRootDirectory);
+            directories.Add(driveRootDirectory);
+            foreach (String dir in dirs)
+            {
+                if (checkPermission(dir))
+                {
+                    directories.Add(dir);
+                    if (Directory.GetDirectories(dir).Length != 0)
+                    {
+                        getAllDirectories(dir);
+                    }
+                }
+
+            }
+        }
+
+        private bool checkPermission(String directory)
+        {
+            try
+            {
+                Directory.GetDirectories(directory);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private bool checkFilePermission(String file)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(file);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private delegate void listDelegate(String file, int lineCount);
+
+        private void updateList(String file, int lineCount)
+        {
+            if (lbResults.InvokeRequired)
+            {
+                listDelegate lstDelegate = new listDelegate(updateList);
+                lbResults.Invoke(lstDelegate, new object[] { file, lineCount });
+
+            }
+            else
+            {
+                lbResults.Items.Add("Found in " + file + " at line: " + lineCount);
+            }
+            
+        }
+        int i = 0;
+        private void pbMakeProgress()
+        {
+            i++;
+            try
+            {
+
+                if (pbSearchProgress.InvokeRequired)
+                {
+                    pbSearchProgress.Invoke(new MethodInvoker(pbMakeProgress));
+                }
+                else
+                {
+                    if (isSearching) pbSearchProgress.Value++;
+                    else pbSearchProgress.Value = 0;
+
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+       
+    }
+}
